b1c485e1aff095e5e6f0ec6e7880f1b3a3335ac1
CrpBnrz <2032217625@qq.com>
2025-12-14 10:05:52 +0800

    Let Forge know that this is a mod

---
+++ src/main/java/crpbnrz/fix13046/Fix13046Mod.java
@@ -0,0 +1,13 @@
+package crpbnrz.fix13046;
+
+import crpbnrz.fix13046.Tags;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+
+
+@Mod(modid = Fix13046Mod.MODID, version = Tags.VERSION, name = "MC-13046 Fix", acceptedMinecraftVersions = "[1.7.10]")
+public class Fix13046Mod {
+    public static final String MODID = "fix13046";
+}
