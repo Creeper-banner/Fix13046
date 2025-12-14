77a7de643cd35632be29e38f6ee39b3856fa419b
CrpBnrz <2032217625@qq.com>
2025-12-14 08:39:01 +0800

    We just fix MC-13046 as what Mojang have done in 1.9 (maybe)

---
+++ src/main/java/crpbnrz/fix13046/mixin/MixinFontRenderer.java
@@ -0,0 +1,21 @@
+package crpbnrz.fix13046.mixin;
+
+import net.minecraft.client.gui.FontRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(FontRenderer.class)
+public class MixinFontRenderer {
+    @ModifyVariable(method = "getCharWidth", at = @At("STORE"), name = "j")
+    private int modifyJ(int j0) {
+        System.out.println(j0);
+        return j0 & 0b1111;
+    }
+
+    @ModifyVariable(method = "renderUnicodeChar", at = @At("STORE"), name = "j")
+    private int modifyJ2(int j0) {
+        System.out.println(j0);
+        return j0 & 0b1111;
+    }
+}
